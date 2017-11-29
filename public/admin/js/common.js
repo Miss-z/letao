d02eaf472cb29da63176912931e2e7384be1387b
miss-z <2801996682@qq.com>
2017-11-29 20:33:54 +0800

    添加了进度条功能

---
+++ public/admin/js/common.js
@@ -0,0 +1,23 @@
+//关闭进度环
+NProgress.configure({
+    showSpinner: false
+})
+$(document).ajaxStart(function(){
+    //开始进度条
+    NProgress.start();
+
+});
+
+$(document).ajaxStop(function(){
+    
+    //结束进度条
+    setTimeout(function(){
+        NProgress.done();
+    },500);
+   
+});
+// 进度条模块结束 
+
+
+
+
