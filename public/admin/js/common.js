4a55915f65b501988a505ba0ec1408b8afb08c89
miss-z <2801996682@qq.com>
2017-11-29 22:06:09 +0800

    首页完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -19,5 +19,38 @@ $(document).ajaxStop(function(){
 // 进度条模块结束 
 
 
+//二级菜单显示和隐藏效果
+$(".child").prev().on("click",function(){
+    $(this).next().slideToggle();
+});
 
+//侧边栏显示与隐藏效果
+$(".icon_menu").on("click",function(){
+    $(".lt_aside").toggleClass("now");
+    $(".lt_main").toggleClass("now");
+});
+
+// 退出功能
+$(".icon_logout").on("click",function(){
+    $("#logoutModal").modal("show");
+
+    //jquery注册事件不会覆盖
+    //.off解绑所有事件
+
+    //注册退出 发送ajax请求  回到登录页
+    $(".btn_logout").off().on("click",function(){
+        //发送ajax请求 告诉服务器退出
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function(data){
+                if(data.success){
+                //退出成功，才跳转到登录页面
+                location.href = "login.html";
+              }
+            }
+        });
+      
+    });
+});
 
